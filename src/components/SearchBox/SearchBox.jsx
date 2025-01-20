c01bf99dfa67211bc2f1c0e7afde68000d139ade
Oksana <oksanavasilevna77@gmail.com>
2025-01-20 18:32:34 +0200

    fix styles

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,8 +3,8 @@ import s from "./SearchBox.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const SearchBox = () => {
- const dispatch = useDispatch()
-const filter = useSelector((state) => state.filters.name);
+  const dispatch = useDispatch();
+  const filter = useSelector((state) => state.filters.name);
   return (
     <div className={s.searchBox}>
       <p className={s.text}>Find contacts by name</p>
@@ -12,7 +12,7 @@ const filter = useSelector((state) => state.filters.name);
         className={s.searchInput}
         type="text"
         value={filter}
-        onChange={(e)=>dispatch(changeFilter(e.target.value))}
+        onChange={(e) => dispatch(changeFilter(e.target.value))}
       />
     </div>
   );
